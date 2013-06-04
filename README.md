2b9b2279f5b8986cdc8735c90a651bd42d3fbd72
Jaanus Uudmäe <jaanus@uudmae.com>
2013-06-04 21:28:52 +0300

    Add missing files. Modify the README file.

---
+++ README.md
@@ -0,0 +1,4 @@
+== Welcome to Rails
+
+This is the first application for
+[*Ruby on Rails Tutorial*]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '617a6bd725d3f7233230b4eff0b8fba2d7a6ed467dcacbba430b8110d64cb32368f88ce06c280ad786cd5243880f79a1b66c8f050685701665d3639ed7b62d1d'
